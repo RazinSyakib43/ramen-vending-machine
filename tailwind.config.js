3a5b1fe544af2418c71dd4799984e3ec3f4ef279
Razin Syakib <muhammadrazin14@gmail.com>
2023-12-12 08:59:33 +0700

    Card v1

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: ["index.html"],
   theme: {
-    extend: {},
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      colors: {
+        primary: "#14b8a6",
+        secondary: "#64748b",
+        dark: "#0f172a",
+      },
+      screens: {
+        "2xl": "1320px",
+        xs: { max: "768px" },
+        // => @media (max-width: 768px) { ... }
+      },
+    },
   },
   plugins: [],
 };
